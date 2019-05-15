c6b7685198a413782470bbd3fd1283f1b76ad73e
YanPanMichael <yanpancsu@icloud.com>
2019-05-16 00:47:24 +0800

    two mether of prototype to implement link chain invokation

--- linkTrain/linkTrain.js
+++ linkTrain/linkTrain.js
@@ -15,3 +15,42 @@ function newPerson() {
 }
 
 console.log(newPerson().set('Marry').get());
+
+// utilize return obj to implement link chain invoke
+function a() {
+  console.log('a');
+  return {
+    b: function() {
+      console.log('b');
+      return {
+        c: function() {
+          console.log('c');
+        }
+      }
+    }
+  }
+}
+a().b().c()
+
+// utilize prototype to implement link chain invoke
+function x() {
+  return new s();
+}
+function s() {
+  console.log('x');
+}
+s.prototype = {
+  y: function() {
+    console.log('y');
+    return this;
+  },
+  z: function() {
+    console.log('z');
+    return this;
+  },
+  o: function() {
+    console.log('o');
+    return this;
+  }
+}
+x().y().z().o()
