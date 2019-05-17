f31cb3fe67552ac4a813cdb0992c3d01cfa73707
YanPanMichael <yanpancsu@icloud.com>
2019-05-18 01:22:56 +0800

    extends

--- extendDemo/extendDemo.js
+++ extendDemo/extendDemo.js
@@ -18,3 +18,38 @@ child1.name = "children";
 
 console.log(child1.name, child2.name);
 console.log(new children().__proto__ instanceof parent);
+
+function Animal() {
+  this.name = "Animal"
+}
+
+function Dog() {
+  this.bark = function() {
+    console.log('Dog bark!');
+  }
+}
+
+Dog.prototype = new Animal();
+new Dog().bark();
+console.log(new Dog().name);
+
+class Animal2 {
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    console.log(this.name+"eat");
+  }
+}
+class Dog2 extends Animal2 {
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+  bark() {
+    console.log(this.name+"bark");
+  }
+}
+const dog = new Dog2('jinmao');
+dog.eat();
+dog.bark();
