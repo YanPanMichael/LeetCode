a2027c1e267628bf2d42749ed04eb5c47ebb2da4
YanPanMichael <yanpancsu@icloud.com>
2019-07-08 23:53:18 +0800

    two demination array to average course score

---
+++ two-dimentional_Array/index.js
@@ -0,0 +1,29 @@
+var grades = [[89,78,99], [77,85,63], [90,92,89]]; //行表示一个学生的3科成绩
+
+//求学生的平均成绩
+(function () {
+  var everScoresArray = [];
+  var total = 0;
+  var average = 0.0;
+  for (var row = 0; row < grades.length; row++) {
+    for (var col = 0; col < grades[row].length; col++) {
+      total += grades[row][col];
+    }
+    average = total / grades[row].length;
+    everScoresArray.push(average);
+    total = 0;
+    average = 0.0;
+  }
+  console.log(everScoresArray.toString());
+})()
+
+//求各科的平均成绩
+(function() {
+  for (let row = 0; row < grades.length; row++) {
+    const person = grades[row];
+    for (let col = 0; col < grades[row].length; col++) {
+      const course = grades[row][col];
+
+    }
+  }
+}())
