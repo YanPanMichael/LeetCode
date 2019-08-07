9ea631f2fe1da5d7ec5d7c4d73cf477436f488ac
YanPanMichael <yanpancsu@icloud.com>
2019-08-08 00:07:58 +0800

    save

--- 2018MajorQuestions/deleteArrayMutiple/deleteArrayMutiple.js
+++ 2018MajorQuestions/deleteArrayMutiple/deleteArrayMutiple.js
@@ -1,12 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var removeDuplicates = function(nums) {
   if(nums.length == 0) return 0;
   let i = 0;
   for(j=1;j<nums.length;j++) {
       if(nums[j] !== nums[i]) {
-          i+=1;
-          nums[i] = num[j]
-      } else {
-          j+=1;
+          i++;
+          nums[i] = nums[j]
       }
   }
+  return i+1;
 };
