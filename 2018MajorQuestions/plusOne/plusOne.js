eca29357408cd2262f3d5280a64a14f75d353d69
YanPanMichael <yanpancsu@icloud.com>
2019-08-09 01:33:53 +0800

    save

--- 2018MajorQuestions/plusOne/plusOne.js
+++ 2018MajorQuestions/plusOne/plusOne.js
@@ -10,11 +10,11 @@ var plusOne = function(digits) {
   digits[low] += 1;
   if (digits[low] > threld){   //大于9  产生进位
       let i = low;
-      while(i > 0 && digits[i] > threld){
+      while(i > high && digits[i] > threld){
           digits[i] = 0;
           digits[--i] += 1;
       }
-      if (i == 0 && digits[i] > threld){   //如果首位大于9， 将数组扩大一位，首位变为1
+      if (i == high && digits[i] > threld){   //如果首位大于9， 将数组扩大一位，首位变为1
           digits[i] = 0;
           digits = [1, ...digits];
       }
