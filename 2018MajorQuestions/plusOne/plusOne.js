d3a2573c89e944a3a4ba5009a3dc90d4c034749b
YanPanMichael <yanpancsu@icloud.com>
2019-08-09 01:33:18 +0800

    plus one in array

---
+++ 2018MajorQuestions/plusOne/plusOne.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  const len = digits.length;
+  let low = len-1;
+  const high = 0;
+  const threld = 9;
+  digits[low] += 1;
+  if (digits[low] > threld){   //大于9  产生进位
+      let i = low;
+      while(i > 0 && digits[i] > threld){
+          digits[i] = 0;
+          digits[--i] += 1;
+      }
+      if (i == 0 && digits[i] > threld){   //如果首位大于9， 将数组扩大一位，首位变为1
+          digits[i] = 0;
+          digits = [1, ...digits];
+      }
+  }
+  return digits;
+};
