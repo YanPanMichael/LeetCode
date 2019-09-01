bae48a82fc93fbf68b39766e46248c64eb0566e1
YanPanMichael <yanpancsu@icloud.com>
2019-09-01 23:36:27 +0800

    数组去重

--- 数组去重/数组去重.js
+++ 数组去重/数组去重.js
@@ -1,5 +1,5 @@
 //思路二：for ... of + includes
-const unique = arr => {
+const unique2 = arr => {
   const res = [];
   for(const item of arr) {
     !res.includes(item) && res.push(item);
@@ -8,5 +8,31 @@ const unique = arr => {
   return res;
 }
 
-const test = [4,4,4,4,3,3,2,2,1];
-console.log(unique(test))
+//Set
+const unique3 = arr => {
+  return [...new Set(arr)];
+  // return Array.from(new Set(arr));
+}
+
+//filter + indexOf
+const unique4 = arr => {
+  return arr.filter((item, index) => {
+    return arr.indexOf(item) === index;
+  })
+}
+
+//for ... of + Object
+const unique5 = arr => {
+  const res = [];
+  const obj = {};
+  for(const item of arr) {
+    if(!obj[item]) {
+      res.push(item);
+      obj[item] = true;
+    }
+  }
+  return res;
+}
+
+const test = [5,6,7,7,5,7,4,4,4,4,3,3,2,2,1];
+console.log(unique5(test))
