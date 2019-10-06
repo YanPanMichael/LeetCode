6694c13dd59ced82956eda11d3c07cb7fbd08acf
YanPanMichael <yanpancsu@icloud.com>
2019-10-07 00:03:32 +0800

    save

--- Z字型变换/Z字型变换.js
+++ Z字型变换/Z字型变换.js
@@ -29,6 +29,44 @@
 // E C   I H   N
 // T     S     G
 
+// 思路
+// 标签：字符串
+// 整体的思路是遍历字符串，遍历过程中将每行都看成新的字符串构成字符串数组，最后再将该数组拼接起来即可
+// 如果 numRows=1numRows=1 则说明当前字符串即为结果，直接返回
+// 否则整个字符串需要经历，向下向右，向下向右，这样的反复循环过程，设定 downdown 变量表示是否向下，locloc 变量表示当前字符串数组的下标
+// 如果 downdown 为 truetrue，则 loc+=1loc+=1，字符串数组下标向后移动，将当前字符加入当前字符串中
+// 如果 downdown 为 falsefalse，则表示向右，则 loc-=1loc−=1，字符串数组下标向前移动，将当前字符加入当前字符串中
+// 时间复杂度：O(n)O(n)，nn为字符串s的长度
+// 代码
+// javajavascript
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows == 1) return s;
+
+  const len = Math.min(s.length, numRows);
+  const rows = [];
+  for (let i = 0; i < len; i++) rows[i] = "";
+  let loc = 0;
+  let down = false;
+
+  for (const c of s) {
+    rows[loc] += c;
+    if (loc == 0 || loc == numRows - 1) down = !down;
+    loc += down ? 1 : -1;
+  }
+
+  let ans = "";
+  for (const row of rows) {
+    ans += row;
+  }
+  return ans;
+};
+
+// 作者：guanpengchn
+// 链接：https://leetcode-cn.com/problems/zigzag-conversion/solution/hua-jie-suan-fa-6-z-zi-xing-bian-huan-by-guanpengc/
 // 来源：力扣（LeetCode）
-// 链接：https://leetcode-cn.com/problems/zigzag-conversion
-// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
