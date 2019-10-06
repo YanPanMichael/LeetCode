8018b41929593ad9075f9921c4c2ce48073dc49c
YanPanMichael <yanpancsu@icloud.com>
2019-10-06 23:59:41 +0800

    save

--- Z字型变换/Z字型变换.js
+++ Z字型变换/Z字型变换.js
@@ -10,3 +10,25 @@
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode-cn.com/problems/zigzag-conversion
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 请你实现这个将字符串进行指定行数变换的函数：
+
+// string convert(string s, int numRows);
+// 示例 1:
+
+// 输入: s = "LEETCODEISHIRING", numRows = 3
+// 输出: "LCIRETOESIIGEDHN"
+// 示例 2:
+
+// 输入: s = "LEETCODEISHIRING", numRows = 4
+// 输出: "LDREOEIIECIHNTSG"
+// 解释:
+
+// L     D     R
+// E   O E   I I
+// E C   I H   N
+// T     S     G
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/zigzag-conversion
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
