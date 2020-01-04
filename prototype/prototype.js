d6fddc10cbab4b9052f850f4d0030baf0312b1e1
YanPanMichael <yanpancsu@icloud.com>
2020-01-05 00:46:31 +0800

    Object.prototype.__proto__ === null

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,8 +1,17 @@
 var a = [];
 var b = function(){}
 b.prototype = a;
+// b.prototype = a.__proto__;
 var c = new b();
 c.push('1')
 console.log(c.length) // 1
 console.log(b.length) // 0
 console.log(a.length) // 0
+
+
+function X() {}
+var x = new X();
+x.__proto__ === X.prototype //true
+x.__proto__.__proto__ === Object.prototype //true
+x.__proto__.__proto__.__proto__ === null //true
+Object.prototype.__proto__ === null //true
