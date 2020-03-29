aae2363c2ef1988e127be19e74b2ce33169a2ddd
YanPanMichael <yanpancsu@icloud.com>
2020-03-29 23:57:24 +0800

    trim

--- createObject/createObject.js
+++ createObject/createObject.js
@@ -609,26 +609,26 @@ function expandArray(inputArray) {
 }
 
 //排列组合 permutation
-var arr = [1, 2, 3];
+const arr = [1, 2, 3, 4, 5];
 // 临时变量，用于输出
-var temp = [];
-function n(arr) {
-  for (var i = 0; i < arr.length; i++) {
+let temp = [];
+function permutation(inputArray) {
+  for (let i = 0; i < inputArray.length; i++) {
     // 插入第i个值
-    temp.push(arr[i]);
+    temp.push(inputArray[i]);
     // 复制数组
-    var copy = arr.slice();
+    let copy = inputArray.slice();
     // 删除复制数组中的第i个值，用于递归
     copy.splice(i, 1);
-    if (copy.length == 0) {
+    if (copy.length === 0) {
       // 如果复制数组长度为0了，则打印变量
       console.log(temp);
     } else {
       // 否则进行递归
-      n(copy);
+      permutation(copy);
     }
     // 递归完了之后删除最后一个元素，保证下一次插入的时候没有上一次的元素
     temp.pop();
   }
 }
-n(arr);
+permutation(arr);
